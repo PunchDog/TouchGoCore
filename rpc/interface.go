df48dd7a858c23584161c51b0088f5cbc3f1b5af
PunchDog <sc317122@hotmail.com>
2023-04-18 18:31:01 +0800

    修改rpc server

--- rpc/interface.go
+++ rpc/interface.go
@@ -5,7 +5,7 @@ import "github.com/golang/protobuf/proto"
 type RpcRequest struct {
 	Websocket      bool          //最后一层消息发向websocket发出去
 	ConnUid        int64         //来的时候的连接ID
-	RemoteServerId int           //远端服务器ID，默认为1
+	RemoteServerId []int         //远端服务器ID，默认为1
 	ForwardingIdx  int8          //转发消息的层次
 	Params         []interface{} //附加数据，比如玩家ID之类的
 	protocol1      int32         //协议号1
--- rpc/server.go
+++ rpc/server.go
@@ -17,17 +17,26 @@ const (
 
 var requestMsg chan *rpc.Call
 
-// 判断是否是自己服务器用的协议
-func serverMsgToServerName(protocol1, protocol2 int32, ForwardingIdx int8) (string, int8) {
-	//获取协议所在的链
+// 判断是否是自己服务器用的协议,ForwardingIdx==-1获取最后一个
+func serverMsgToServerName(protocol1, protocol2 int32, ForwardingIdx int8) string {
 	key := fmt.Sprintf("%d-%d", protocol1, protocol2)
+	//如果是-1，就获取最后一个键值
+	if ForwardingIdx == -1 {
+		len := redis_.Get().LLen(key).Val()
+		if len > 0 {
+			ForwardingIdx = int8(len) - 1
+		} else {
+			//出错了
+			return ""
+		}
+	}
+	//获取协议所在的链
 	d := redis_.Get().LIndex(key, int64(ForwardingIdx))
 	if serverName, err := d.Result(); err == nil {
-		maxLen := redis_.Get().LLen(key).Val()
-		return serverName, int8(maxLen)
+		return serverName
 	}
 
-	return "", 0
+	return ""
 }
 
 type RpcServer struct {
@@ -43,7 +52,7 @@ func (self *RpcServer) RegisterClient(args *RpcRequest, reply *registerClient) e
 
 // 消息广播
 func (self *RpcServer) MsgDispatch(args *RpcRequest, reply *RpcResponse) error {
-	serverName, maxLen := serverMsgToServerName(args.protocol1, args.protocol2, args.ForwardingIdx) //获取协议所在的服务器名字
+	serverName := serverMsgToServerName(args.protocol1, args.protocol2, -1) //获取协议所在的服务器名字
 	if serverName == "" {
 		vars.Error(fmt.Sprintf("服务器名字获取错误，目前没有协议%d-%d对应的服务器", args.protocol1, args.protocol2))
 		return nil
@@ -59,23 +68,27 @@ func (self *RpcServer) MsgDispatch(args *RpcRequest, reply *RpcResponse) error {
 			}
 		}
 	} else {
-		if args.ForwardingIdx < maxLen {
-			args.ForwardingIdx++ //转发层级+1，看下次转发到哪,如果已经是最后一层了，那就是往客户端的websocket发展了
+		args.ForwardingIdx++                                                                    //转发层级+1，看下次转发到哪,如果已经是最后一层了，那就是往客户端的websocket发展了
+		serverName := serverMsgToServerName(args.protocol1, args.protocol2, args.ForwardingIdx) //获取协议所在的服务器名字
+		if serverName != "" {
 			//不是本服的协议，需要查询协议目的地，转过去
-			conn := GetConn(serverName, args.RemoteServerId)
+			conn := GetConn(serverName, args.RemoteServerId[args.ForwardingIdx])
 			if conn != nil {
 				conn.Go(DISPATCH, args, reply) //消息转发
 			} else {
 				//出错了，获取不到对应的连接
 				reply.Error = -1
 			}
+		} else {
+			//出错了，获取不到对应的连接
+			reply.Error = -1
 		}
 	}
 	return nil
 }
 
 /*
-注册协议链,即协议转发几个服务器(Protocol1-Protocol2=>[]string{server1,server2,...})
+注册协议链,即协议转发几个服务器((Protocol1-Protocol2)=>[]string{server1,server2,...})，有几个服务器要转发，就要填几个
 其实就是设置协议在哪个服务器里执行
 */
 func RegiserProtocolServerNameList(mp *syncmap.Map) {

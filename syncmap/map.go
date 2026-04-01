0c779d50b2d85170e4118d2c0e5148c134096cbe
PunchDog <sc317122@hotmail.com>
2026-04-01 14:06:53 +0800

    1

--- syncmap/map.go
+++ syncmap/map.go
@@ -40,7 +40,7 @@ func (this *Map) Delete(k any) {
 }
 
 // 清空所有数据（不可以在fn内有对this的Store或者Delete操作）
-func (this *Map) ClearAll(fn func(k, v interface{}) bool) {
+func (this *Map) ClearAll(fn func(k, v any) bool) {
 	this.mu.Lock()
 	defer this.mu.Unlock()
 	for k, v := range this.mp {
@@ -53,7 +53,7 @@ func (this *Map) ClearAll(fn func(k, v interface{}) bool) {
 }
 
 // 添加或读取
-func (this *Map) LoadOrStore(key, value interface{}) (actual interface{}, loaded bool) {
+func (this *Map) LoadOrStore(key, value any) (actual any, loaded bool) {
 	this.mu.Lock()
 	defer this.mu.Unlock()
 	if this.mp == nil {
@@ -77,7 +77,7 @@ func (this *Map) Load(k any) (v any, ok bool) {
 }
 
 // 循环
-func (this *Map) Range(fn func(k, v interface{}) bool) {
+func (this *Map) Range(fn func(k, v any) bool) {
 	this.mu.RLock()
 	defer this.mu.RUnlock()
 	for k, v := range this.mp {
